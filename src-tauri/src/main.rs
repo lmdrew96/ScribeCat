62e8f46a706999d1cba609221b48b8bddc3e8cf3
Nae <lmdrew@udel.edu>
2025-09-16 21:38:59 -0400

    git-commit

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,18 @@
-#![cfg_attr(all(not(debug_assertions), target_os = "windows"), windows_subsystem = "windows")]
+// src-tauri/src/main.rs
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // harmless on macOS
 
+fn main() {
+  // In dev, load env from .env (src-tauri/.env or project-root/.env)
+  #[cfg(debug_assertions)]
+  {
+    let _ = dotenvy::from_filename(".env")
+      .or_else(|_| dotenvy::from_filename("../.env"));
+  }
+
+  tauri::Builder::default()
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+}
 use std::fs::{remove_file, OpenOptions};
 use std::path::PathBuf;
 use std::process::{Child, Command, Stdio};
